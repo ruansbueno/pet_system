b5584212dae25774e5731060b7cf336aee3bccd1
ruansbueno <ruansbueno@gmail.com>
2025-09-20 20:14:51 -0300

    Correção de seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,8 +18,8 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call([
-            Products::class,
             Users::class,
+            Products::class,
         ]);
     }
 }
